56592c542fb3ff2ead327cf6fb2f4f7801b98ad3
Yair Szarf <yair@mac.com>
2016-08-16 22:15:01 -0400

    Configuring Environment Variables Part 3

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'the2handedcodingparsetest',
   masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://the2handedcodingparsetest.herokuapp.com/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://the2handedcodingparsetest.herokuapp.com/1',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
